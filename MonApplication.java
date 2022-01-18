918b212b4c0870e0bffb7fe14bbbc16226d5dd22
Cannel Seka <cannelseka@gmail.com>
2022-01-18 11:01:17 +0000

    First commit

---
+++ MonApplication.java
@@ -0,0 +1,16 @@
+public class MonApplication {
+
+    public static void main(String arg[]){
+
+        System.out.println("demo condition");
+
+        int i = 10;  
+
+        if(i<0)
+            System.out.println("Le nombre est négatif");
+        else
+            System.out.println("Le nombre est positif");
+
+    }
+
+}
